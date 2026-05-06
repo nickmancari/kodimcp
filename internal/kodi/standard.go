621ed07b5ab472c342514ade6e13d87957c031e7
Nick Mancari <nickjmancari@gmail.com>
2026-05-06 16:49:24 -0500

    more baseline tools

--- internal/kodi/standard.go
+++ internal/kodi/standard.go
@@ -1,10 +1,25 @@
 package kodi
 
-import (
-)
-
 func (c *KodiClient) Ping() (any, error) {
 	res, err := c.Call("JSONRPC.Ping", map[string]any{})
+	if err != nil {
+		return nil, err
+	}
+
+	return res["result"], nil
+}
+
+func (c *KodiClient) Shutdown() (any, error) {
+	res, err := c.Call("System.Shutdown", map[string]any{})
+	if err != nil {
+		return nil, err
+	}
+
+	return res["result"], nil
+}
+
+func (c *KodiClient) Reboot() (any, error) {
+	res, err := c.Call("System.Reboot", map[string]any{})
 	if err != nil {
 		return nil, err
 	}
